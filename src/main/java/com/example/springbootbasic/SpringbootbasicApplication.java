869626c6af2260f8bf45f7ce455ef9fd9b3447db
TAESIC KONG <xotlr333@naver.com>
2023-07-07 00:19:29 +0900

    init

---
+++ src/main/java/com/example/springbootbasic/SpringbootbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootbasicApplication.class, args);
+    }
+
+}
