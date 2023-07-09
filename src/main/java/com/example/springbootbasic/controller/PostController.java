b628ea895a8a6337537916c1afe47a79047fa4a9
kongtaesic <xotlr333@naver.com>
2023-07-09 23:32:07 +0900

    5.3 POST API: 1. @RequestMapping 활용
    GET API와 동일하게 POST API도 @RequestMapping로 구현했다.

---
+++ src/main/java/com/example/springbootbasic/controller/PostController.java
@@ -0,0 +1,15 @@
+package com.example.springbootbasic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    @RequestMapping(value = "/domain", method = RequestMethod.POST)
+    public String postExample() {
+        return "Hello POST API";
+    }
+}
