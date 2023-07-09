1a42484d31932f95403a68f5b23f39b6399194cf
kongtaesic <xotlr333@naver.com>
2023-07-09 23:18:54 +0900

    5.2 GET API: 4. @RequestParam 활용
    쿼리스트링의 값을 매개변수로 받을 수 있는 @RequestParam를 활용하여 메소드를 구현했다.
    만약, 어떤 값을 받을지 정할 수 없는 경우는 Map 객체를 활용하면 모든 쿼리스트링을 받을 수 있다.

--- src/main/java/com/example/springbootbasic/controller/GetController.java
+++ src/main/java/com/example/springbootbasic/controller/GetController.java
@@ -2,6 +2,8 @@ package com.example.springbootbasic.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
@@ -24,5 +26,24 @@ public class GetController {
     @GetMapping(value = "/variable2/{variable}")
     public String getVariable2(@PathVariable(name = "variable") String var) {
         return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization
+    ) {
+        return name + " " + email + " " + organization;
+    }
+
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
     }
 }
