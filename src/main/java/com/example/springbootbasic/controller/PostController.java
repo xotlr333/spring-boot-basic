08a29fc9b1c9235df875bd1be099d3795f76275b
kongtaesic <xotlr333@naver.com>
2023-07-09 23:39:55 +0900

    5.3 POST API: 2. @RequestBody 활용
    POST API는 body에 데이터를 담는다. body의 데이터를 가져올 수 있는 @RequestBody를 활용하여 메소드를 구현했다.
    @RequestBody도 DTO 객체를 활용하여 데이터를 가져올 수 있다.

--- src/main/java/com/example/springbootbasic/controller/PostController.java
+++ src/main/java/com/example/springbootbasic/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.example.springbootbasic.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springbootbasic.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -11,5 +12,21 @@ public class PostController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello POST API";
+    }
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMember2(@RequestBody MemberDto memberDto) {
+
+        return memberDto.toString();
     }
 }
