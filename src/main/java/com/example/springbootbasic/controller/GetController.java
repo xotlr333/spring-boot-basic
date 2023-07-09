158b302a9caf9119e44174710a9ac4a358a14f15
kongtaesic <xotlr333@naver.com>
2023-07-09 22:56:48 +0900

    5.2 GET API: 1. @RequestMapping으로 구현
    @RequestMapping을 이용해서 GET API를 구현했다.
    하지만 스프링 4.3 버전 이후로는 @RequestMapping를 더 이상 사용하지 않는다.
    대신 @GetMapping, @PostMapping, @PutMapping, @DeleteMapping을 사용한다.

---
+++ src/main/java/com/example/springbootbasic/controller/GetController.java
@@ -0,0 +1,15 @@
+package com.example.springbootbasic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+}
