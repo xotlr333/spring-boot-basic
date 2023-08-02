c9812280ad41835f1bae28a91da5af273d7d0f9e
kongtaesic <xotlr333@naver.com>
2023-08-03 01:20:23 +0900

    7.4 테스트 코드 작성: 6. 컨트롤러 테스트 코드 작성
    productcontroller의 getProduct 메소드에 대한 테스트 코드를 작성했다.
    Mockito에서 제공하는 given()를 통해 호출하는 메소드에 대한 설정을 해준다.
    mocMvc를 이용해 실제 결과값을 확인하는 코드를 작성한다.
    verify()는 지정된 메소드가 실행되었는지 확인하는 용도이다.

---
+++ src/test/java/com/example/springbootbasic/controller/ProductControllerTest.java
@@ -0,0 +1,50 @@
+package com.example.springbootbasic.controller;
+
+import com.example.springbootbasic.data.dto.ProductResponseDto;
+import com.example.springbootbasic.service.impl.ProductServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ProductServiceImpl productService;
+
+    @Test
+    @DisplayName("MockMvc를 통한 Product 데이터 가져오기 테스트")
+    void getProductTest() throws Exception {
+
+        given(productService.getProduct(123L)).willReturn(
+                new ProductResponseDto(123L, "pen", 500, 400)
+        );
+
+        String productId = "123";
+
+        mockMvc.perform(
+                get("/product?number=" + productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.number").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andDo(print());
+
+        verify(productService).getProduct(123L);
+    }
+
+}
